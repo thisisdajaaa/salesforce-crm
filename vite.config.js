d6140e50120be4c3966eb87eb5f41fe9a18217ae
DAJA <33202282+DAJAKMPM@users.noreply.github.com>
2025-03-04 21:54:10 -0600

    wip: ongoing config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/salesforce-crm/", // Set this to match your repository name
+});
